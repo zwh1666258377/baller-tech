6dd87aa663ed9336c4e58c3b11dbb99758b46b32
東澔 <donghaozhao_a2@163.com>
2020-10-02 10:22:19 +0800

    modify api whitelist

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -1,4 +1,4 @@
-const filterList = ['/api/login'];
+const filterList = ['/api/login', '/api/get-module'];
 
 export function WhiteList(url: string): boolean {
   return !!filterList.find(filterUrl => url.indexOf(filterUrl) > -1);
