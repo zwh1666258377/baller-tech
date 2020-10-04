6a3037a54e3e52d599b6794d1b6bcf620dc9cdd0
東澔 <donghaozhao_a2@163.com>
2020-10-04 12:25:44 +0800

    _

--- src/pages/h5/layout.tsx
+++ src/pages/h5/layout.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { configResponsive, useResponsive } from 'ahooks';
+import { useHistory } from 'umi';
+
+configResponsive({
+  isPC: 750,
+});
 
 export default ({ children }) => {
+  const { isPC } = useResponsive();
+  const { push } = useHistory();
+
+  React.useEffect(() => {
+    isPC && push('/');
+  }, [isPC]);
+
   return (
     <div>
       <div
