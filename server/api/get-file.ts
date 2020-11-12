9fa5c7c778abdc79fff23ace820dd7c7d0a80f86
doho <zhaodonghao586@outlook.com>
2020-11-12 08:57:27 +0800

    video

--- server/api/get-file.ts
+++ server/api/get-file.ts
@@ -7,14 +7,43 @@ export function getFile(app: ReturnType<typeof express>) {
     const { id } = req.query;
     const doc = await ResourceSchema.findById(id);
     if (!!doc?.path) {
-      const buff = fs.readFileSync(doc?.path);
+      if (doc.mimetype?.startsWith('video/')) {
+        const path = doc?.path;
+        const stat = fs.statSync(path);
+        const fileSize = stat.size;
+        const range = req.headers.range;
+        if (range) {
+          //有range头才使用206状态码
+          const parts = range.replace(/bytes=/, '').split('-');
+          const start = parseInt(parts[0], 10);
+          let end = parts[1] ? parseInt(parts[1], 10) : start + 999999;
 
-      res.attachment(doc?.originalname);
-      // res.set(
-      //   'Content-Disposition',
-      //   `attachment;filename=${doc?.originalname}`,
-      // );
-      res.end(buff);
+          // end 在最后取值为 fileSize - 1
+          end = end > fileSize - 1 ? fileSize - 1 : end;
+
+          const chunksize = end - start + 1;
+          const file = fs.createReadStream(path, { start, end });
+          const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+          };
+          res.writeHead(206, head);
+          file.pipe(res);
+        } else {
+          const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+          };
+          res.writeHead(200, head);
+          fs.createReadStream(path).pipe(res);
+        }
+      } else {
+        const buff = fs.readFileSync(doc?.path);
+        res.attachment(doc?.originalname);
+        res.end(buff);
+      }
     } else {
       res.end();
     }
